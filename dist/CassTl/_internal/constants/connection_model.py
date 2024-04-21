fed267ebfb29e1a27be73e4c767cbd003ceb6d48
Tomas Franek <xfranek@mendelu.cz>
2024-04-21 15:43:27 +0200

    Removed dist

--- dist/CassTl/_internal/constants/connection_model.py
+++
@@ -1,7 +0,0 @@
-from cassandra.cluster import Cluster
-
-
-class Connection(dict):
-    def __init__(self: 'Connection', cluster: Cluster, connection_profile_name: str) -> None:
-        self.cluster = cluster
-        self.connection_profile_name = connection_profile_name
