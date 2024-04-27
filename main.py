d835338a7d28e48dea18fd3c140454d8a4e4662c
Tomas Franek <xfranek@mendelu.cz>
2024-04-27 11:52:41 +0200

    Added icon.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os
 import sys
+from PyQt6 import QtGui
 from PyQt6.QtWidgets import QApplication
 from backend.connection_profiles.authentication_window import AuthenticationWindow
+
+basedir = os.path.dirname(__file__)
 
 
 def main() -> None:
     app = QApplication(sys.argv)
+    app.setWindowIcon(QtGui.QIcon(os.path.join(basedir, 'casstl.ico')))
     authentication_window = AuthenticationWindow()
     authentication_window.authenticate()
     sys.exit(app.exec())
